ddc99916769cafe79e9bc7be7fff952c487be37d
lstritz <leastritzke@gmail.com>
2024-10-10 18:25:32 +0200

    modified:   project.py     Defining variables and data

--- project.py
+++ project.py
@@ -1,1 +1,18 @@
-input()
+main_transport = input('Your options are: car, train, plane')
+print('Main transport:', main_transport)
+travel_days = input('Your average amount of travel days per year:')
+print('The average amount of travel days per year:', travel_days)
+distance = input('Your options are: Domestic, Europe, World')
+print('Distance travelled', distance)
+
+# average values for each mean of transport (kg of carbon per km) reference: Our World in Data
+car_co2 = 0.17 
+train_co2 = 0.04 
+plane_co2 = 0.24 
+
+#average values for distances in km
+Domestic_distance = 100 
+Europe_distance = 500
+World_distance = 6000
+
+
