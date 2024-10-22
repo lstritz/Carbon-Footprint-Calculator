0ad9c1e00667a9cea6a27a119060e5bc08d0a073
lstritz <leastritzke@gmail.com>
2024-10-22 11:30:45 +0200

    function carbon has been split to be able to test for errors

--- project.py
+++ project.py
@@ -1,3 +1,57 @@
+def carbon_car(car_co2, travel_days, distance):
+    carbon_car_days = car_co2 * int(travel_days)
+    if distance == 'Domestic':
+        carbon_transport = carbon_car_days * Domestic_distance
+    elif distance == 'Europe':
+        carbon_transport = carbon_car_days * Europe_distance
+    elif distance == 'World':
+        carbon_transport = carbon_car_days * World_distance
+    else:
+        print('Incorrect value for main transport or distance')
+    return carbon_transport
+
+def carbon_train(train_co2, travel_days, distance):
+    carbon_train_days = train_co2 * int(travel_days)
+    if distance == 'Domestic':
+        carbon_transport = carbon_train_days * Domestic_distance
+    elif distance == 'Europe':
+        carbon_transport = carbon_train_days * Europe_distance
+    elif distance == 'World':
+        carbon_transport = carbon_train_days * World_distance
+    else:  
+        print('Incorrect value for main transport or distance')
+    return carbon_transport
+
+def carbon_plane(plane_co2, travel_days, distance):
+    carbon_plane_days = plane_co2 * int(travel_days)
+    if distance == 'Domestic':
+        carbon_transport = carbon_plane_days * Domestic_distance
+    elif distance == 'Europe':
+        carbon_transport = carbon_plane_days * Europe_distance
+    elif distance == 'World':
+        carbon_transport = carbon_plane_days * World_distance
+    else:
+        print('Incorrect value for main transport or distance')
+    return carbon_transport
+
+def carbon(main_transport, travel_days, distance):
+    if main_transport == 'car':
+        amount = carbon_car(car_co2, travel_days, distance)
+        return amount
+    elif main_transport == 'train':
+        return carbon_train(train_co2, travel_days, distance)
+    elif main_transport == 'plane':
+        return carbon_plane(plane_co2, travel_days, distance)  
+    else:
+        print('Incorrect value for main transport or distance')
+        main_transport = input('Your options are: car, train, plane:')
+        print('Main transport:', main_transport)
+        travel_days = input('Your average amount of travel days per year:')
+        print('The average amount of travel days per year:', travel_days)
+        distance = input('Your options are: Domestic, Europe, World:')
+        print('Distance travelled:', distance)
+    return carbon_final        
+
 main_transport = input('Your options are: car, train, plane:')
 print('Main transport:', main_transport)
 travel_days = input('Your average amount of travel days per year:')
@@ -10,43 +64,10 @@ car_co2 = 0.17
 train_co2 = 0.04 
 plane_co2 = 0.24 
 
-#average values for distances in km
+# average values for distances in km
 Domestic_distance = 100 
 Europe_distance = 500
 World_distance = 6000
-
-def carbon(main_transport, travel_days, distance):
-    if main_transport == 'car':
-        carbon_car = car_co2 * int(travel_days)
-        if distance == 'Domestic':
-            carbon_final = carbon_car * Domestic_distance
-        elif distance == 'Europe':
-            carbon_final = carbon_car * Europe_distance
-        elif distance == 'World':
-            carbon_final = carbon_car * World_distance
-        else:
-            print('Incorrect value for main transport or distance')
-    elif main_transport == 'train':
-        carbon_train = train_co2 * int(travel_days)
-        if distance == 'Domestic':
-            carbon_final = carbon_train * Domestic_distance
-        elif distance == 'Europe':
-            carbon_final = carbon_train * Europe_distance
-        elif distance == 'World':
-            carbon_final = carbon_train * World_distance
-        else:  
-            print('Incorrect value for main transport or distance')
-    elif main_transport == 'plane':
-        carbon_plane = plane_co2 * int(travel_days)
-        if distance == 'Domestic':
-            carbon_final = carbon_plane * Domestic_distance
-        elif distance == 'Europe':
-            carbon_final = carbon_plane * Europe_distance
-        elif distance == 'World':
-            carbon_final = carbon_plane * World_distance
-        else:
-            print('Incorrect value for main transport or distance')  
-    return carbon_final        
 
 carbon_final = carbon(main_transport, travel_days, distance)
 print('Your average carbon footprint', carbon_final)
