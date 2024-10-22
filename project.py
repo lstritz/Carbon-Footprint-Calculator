e279317fe39f51dc934af89e2f472c69fcfd88a9
lstritz <leastritzke@gmail.com>
2024-10-22 13:36:09 +0200

    Added docstrings, limited amount for travel days

--- project.py
+++ project.py
@@ -1,4 +1,14 @@
 def carbon_car(car_co2, travel_days, distance):
+    """Calculates the Carbon footprint for a car user
+
+    Args:
+        car_co2 (int): Average car co2 from Our World in Data
+        travel_days (str): User input, average amount of travel days per year
+        distance (str): User input, Chooses distance travelled 
+
+    Returns:
+        carbon_transport(flt): Average carbon footprint for a car user
+    """
     carbon_car_days = car_co2 * int(travel_days)
     if distance == 'Domestic':
         carbon_transport = carbon_car_days * Domestic_distance
@@ -6,11 +16,19 @@ def carbon_car(car_co2, travel_days, distance):
         carbon_transport = carbon_car_days * Europe_distance
     elif distance == 'World':
         carbon_transport = carbon_car_days * World_distance
-    else:
-        print('Incorrect value for main transport or distance')
     return carbon_transport
 
 def carbon_train(train_co2, travel_days, distance):
+    """Calculates the Carbon footprint for a train user
+
+    Args:
+        train_co2 (int): Average train co2 from Our World in Data
+        travel_days (str): User input, average amount of travel days per year
+        distance (str): User input, Chooses distance travelled
+
+    Returns:
+        carbon_transport(flt): Average carbon footprint for a train user
+    """
     carbon_train_days = train_co2 * int(travel_days)
     if distance == 'Domestic':
         carbon_transport = carbon_train_days * Domestic_distance
@@ -18,11 +36,19 @@ def carbon_train(train_co2, travel_days, distance):
         carbon_transport = carbon_train_days * Europe_distance
     elif distance == 'World':
         carbon_transport = carbon_train_days * World_distance
-    else:  
-        print('Incorrect value for main transport or distance')
     return carbon_transport
 
 def carbon_plane(plane_co2, travel_days, distance):
+    """Calculates the Carbon footprint for a plane user
+
+    Args:
+        plane_co2 (int): Average plane co2 from Our World in Data
+        travel_days (str): User input, average amount of travel days per year
+        distance (str): User input, Chooses distance travelled
+
+    Returns:
+        carbon_transport(flt): Average carbon footprint for a plane user
+    """
     carbon_plane_days = plane_co2 * int(travel_days)
     if distance == 'Domestic':
         carbon_transport = carbon_plane_days * Domestic_distance
@@ -30,20 +56,27 @@ def carbon_plane(plane_co2, travel_days, distance):
         carbon_transport = carbon_plane_days * Europe_distance
     elif distance == 'World':
         carbon_transport = carbon_plane_days * World_distance
-    else:
-        print('Incorrect value for main transport or distance')
     return carbon_transport
 
 def carbon(main_transport, travel_days, distance):
+    """Calculates the Carbon footprint after diffeenciating between the main transport: car, train, plane
+
+    Args:
+        main_transport (str): User input: car, train or plane
+        travel_days (str): User input between 0 and 365
+        distance (str): User input: Domestic, Europe, World
+
+    Returns:
+        carbon_transport(flt): The average carbon footprint per year for one of the transport options
+    """
     if main_transport == 'car':
-        amount = carbon_car(car_co2, travel_days, distance)
-        return amount
+        return carbon_car(car_co2, travel_days, distance)
     elif main_transport == 'train':
         return carbon_train(train_co2, travel_days, distance)
     elif main_transport == 'plane':
         return carbon_plane(plane_co2, travel_days, distance)  
     else:
-        print('Incorrect value for main transport or distance')
+        print('Incorrect value for main transport, travel days or distance. Try again')
         main_transport = input('Your options are: car, train, plane:')
         print('Main transport:', main_transport)
         travel_days = input('Your average amount of travel days per year:')
@@ -52,12 +85,20 @@ def carbon(main_transport, travel_days, distance):
         print('Distance travelled:', distance)
     return carbon_final        
 
+print('This is a simple carbon footprint calculator. Please insert the following values')
+
 main_transport = input('Your options are: car, train, plane:')
 print('Main transport:', main_transport)
+
 travel_days = input('Your average amount of travel days per year:')
-print('The average amount of travel days per year:', travel_days)
+while int(travel_days) <0 or int(travel_days)>366:
+    print('Your chosen amount of travel days per year is invalid. Please try again')
+    travel_days = input('Your average amount of travel days per year:')       
+if int(travel_days)>=0 and int(travel_days)<=365:
+        print('The average amount of travel days per year:', travel_days)
+
 distance = input('Your options are: Domestic, Europe, World:')
-print('Distance travelled:', distance)
+print('Distance travelled:', distance) 
 
 # average values for each mean of transport (kg of carbon per km) reference: Our World in Data
 car_co2 = 0.17 
@@ -70,4 +111,4 @@ Europe_distance = 500
 World_distance = 6000
 
 carbon_final = carbon(main_transport, travel_days, distance)
-print('Your average carbon footprint', carbon_final)
+print('Your average carbon footprint', carbon_final, 'kg of CO2 per year')
